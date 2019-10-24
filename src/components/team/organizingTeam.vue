0abfea4cd7e9b01f47cdd308b6b0ea9b1ee282b3
Roopa Rajala <roopa.rajala@nov.com>
2019-10-23 21:48:27 -0500

    fix bugs
    update team and events page
    
    add GDG Houston template

--- src/components/team/organizingTeam.vue
+++ src/components/team/organizingTeam.vue
@@ -2,18 +2,18 @@
     <v-container class="pa-0 mb-5">
 
         <v-layout wrap align-start justify-start row fill-height class="mt-0 mb-0">
-            <v-flex xs12 class="mb-3">
+           <!-- <v-flex xs12 class="mb-3">
                 <p class="google-font" style="font-size:130%;">Core Organizing Team Members</p>
                 <v-divider></v-divider>
             </v-flex>
             <v-flex xs12 md3 lg3 v-for="(item,i) in TeamDetails" :key="i">
                 <v-list three-line class="pa-2">
                     <template >
-                
+
                     <v-list-tile
                         :key="item.name"
                         avatar
-                        
+
                     >
                         <v-list-tile-avatar>
                             <img :src="getImgUrl(item.profileImage)">
@@ -23,7 +23,7 @@
                             <v-list-tile-title class="google-font" v-html="item.name"></v-list-tile-title>
                             <v-list-tile-sub-title><span class="google-font">{{item.designation}}</span></v-list-tile-sub-title>
                             <v-list-tile-sub-title>
-                                
+
                                 <v-btn class="mt-0 mx-0" icon v-if="(item.twitter).length>0" :href="item.twitter" target="_blank">
                                     <v-icon small style="color:#1da1f2">fab fa-twitter</v-icon>
                                 </v-btn>
@@ -34,18 +34,18 @@
 
                                 <v-btn class="mt-0 mx-0" icon v-if="(item.github).length>0" :href="item.github" target="_blank">
                                     <v-icon small style="color:#333">fab fa-github</v-icon>
-                                </v-btn>
+                                </v-btn> -->
                                 <!-- <v-btn class="mt-0 mx-0" icon v-if="(item.website).length>0" :href="item.website" target="_blank">
                                     <v-icon style="color:#333">link</v-icon>
                                 </v-btn> -->
 
-                            </v-list-tile-sub-title>
+                           <!-- </v-list-tile-sub-title>
                         </v-list-tile-content>
                     </v-list-tile>
                     </template>
                 </v-list>
-            </v-flex>
-         
+            </v-flex> -->
+
         </v-layout>
 
 
@@ -64,7 +64,7 @@ export default {
         }
     },
     mounted(){
-        
+
     },
     methods:{
         getImgUrl(pic) {
