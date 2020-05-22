8fc765fef58fa8f4865451d34e26c9cbb9a637ee
awalakaushik <kawala@uh.edu>
2020-05-21 19:05:18 -0500

    enable the feature event page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,7 +25,7 @@
       </v-layout>
     </v-container>
 
-    <!-- <v-container fluid class="py-5" style="background-color:#F9F9F9">
+    <v-container fluid class="py-5" style="background-color:#F9F9F9">
       <v-layout wrap align-start justify-start row fill-height>
         <v-flex xs12 md10 offset-md1 offset-lg1 >
           <p class="google-font mb-2" style="font-size:150%">Our Upcoming Event & Meetup</p>
@@ -34,7 +34,7 @@
           <featureEvent />
         </v-flex>
       </v-layout>
-    </v-container> -->
+    </v-container>
 
     <v-container fluid >
       <v-layout wrap align-center justify-center row fill-height>
@@ -75,7 +75,7 @@
   import eventshowcase from '@/components/home/eventshowcase'
   // import wtmInfo from '@/components/home/wtmInfo'
   import partners from '@/components/home/partners'
-  // import featureEvent from '@/components/home/featureEvent'
+  import featureEvent from '@/components/home/featureEvent'
   
   export default {
     components: {
@@ -85,7 +85,7 @@
       eventshowcase,
       // wtmInfo,
       partners,
-      // featureEvent
+      featureEvent
     }
   }
 </script>
